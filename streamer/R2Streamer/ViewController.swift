43ddf3ec401653b62e8acd3af1e2bcd500c43b67
Olivier Körner <olivierkorner@abm-studio.com>
2016-12-14 21:54:15 +0100

    Initial Commit

---
+++ streamer/R2Streamer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  R2Streamer
+//
+//  Created by Olivier Körner on 14/12/2016.
+//  Copyright © 2016 Readium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
