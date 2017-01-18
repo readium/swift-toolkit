0dcb876784bc7ab50d5e0cb052b98ae92d7a5ab2
Olivier Körner <olivierkorner@abm-studio.com>
2017-01-18 15:36:35 +0100

    Turned the Xcode project into a Swift framework
    This repository is for the streamer only now.
    The app is in a distinct repository, dependent on this one.
    
    
    Former-commit-id: 0a0b39fb9a1738e99815aefa27c03e50a4361377

---
+++ streamer/R2Streamer/R2Streamer.h
@@ -0,0 +1,19 @@
+//
+//  R2Streamer.h
+//  R2Streamer
+//
+//  Created by Olivier Körner on 18/01/2017.
+//  Copyright © 2017 Readium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for R2Streamer.
+FOUNDATION_EXPORT double R2StreamerVersionNumber;
+
+//! Project version string for R2Streamer.
+FOUNDATION_EXPORT const unsigned char R2StreamerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <R2Streamer/PublicHeader.h>
+
+
