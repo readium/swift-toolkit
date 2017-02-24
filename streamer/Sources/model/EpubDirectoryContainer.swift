8a865c9ea08db65d4e620a45378b3ac1d3fe03bf
Alexandre Camilleri <alexandre.camilleri@edrlab.com>
2017-02-24 11:59:37 +0100

    rename DirectoryContainer.swift to EpubDirectoryContainer.swift
    Former-commit-id: 953564c931e6f0c3bbc82deddc2d182f0e127070

---
+++ streamer/Sources/model/EpubDirectoryContainer.swift
@@ -0,0 +1,74 @@
+//
+//  DirectoryContainer.swift
+//  R2Streamer
+//
+//  Created by Alexandre Camilleri on 2/15/17.
+//  Copyright © 2017 Readium. All rights reserved.
+//
+
+import Foundation
+
+/// EPUB Container for EPUBs unzipped in a directory.
+open class EpubDirectoryContainer: Container {
+    
+    /// The root directory path
+    var rootPath: String
+
+    public init?(directory dirPath: String) {
+        guard FileManager.default.fileExists(atPath: dirPath) else {
+            return nil
+        }
+        rootPath = dirPath
+    }
+    
+    open func data(relativePath: String) throws -> Data {
+        let fullPath = generateFullPath(with: relativePath)
+
+        return try Data(contentsOf: URL(fileURLWithPath: fullPath), options: [.mappedIfSafe])
+    }
+
+
+    /// <#Description#>
+    ///
+    /// - Parameter relativePath: <#relativePath description#>
+    /// - Returns: <#return value description#>
+    /// - Throws: <#throws value description#>
+    open func dataLength(relativePath: String) throws -> UInt64 {
+        let fullPath = generateFullPath(with: relativePath)
+
+        guard let attributes = try? FileManager.default.attributesOfItem(atPath: fullPath) else {
+            throw ContainerError.fileError
+        }
+        guard let fileSize = attributes[FileAttributeKey.size] as? UInt64 else {
+            throw ContainerError.fileError
+        }
+        return fileSize
+    }
+
+
+    /// <#Description#>
+    ///
+    /// - Parameter relativePath: <#relativePath description#>
+    /// - Returns: <#return value description#>
+    /// - Throws: <#throws value description#>
+    open func dataInputStream(relativePath: String) throws -> SeekableInputStream {
+        let fullPath = generateFullPath(with: relativePath)
+
+        guard let inputStream = FileInputStream(fileAtPath: fullPath) else {
+            throw ContainerError.streamInitFailed
+        }
+        return inputStream
+    }
+
+
+    /// Generate an absolute path to a ressource from a given relative path.
+    ///
+    /// - Parameter relativePath: The 'directory-relative' path to the ressource.
+    /// - Returns: The absolute path to the ressource
+    internal func generateFullPath(with relativePath: String) -> String {
+        let fullPath: String
+
+        fullPath = (rootPath as NSString).appendingPathComponent(relativePath)
+        return fullPath
+    }
+}
