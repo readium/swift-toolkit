d52ab49944fae9abd8fe9e4aa13122ce125fd41f
Alexandre Camilleri <alexandre.camilleri@edrlab.com>
2017-03-01 16:03:33 +0100

    Adding comments
    Former-commit-id: 7f93f17719d40c8f0bdd4c0c3bdccfbfbe5e6ba7

--- streamer/Sources/Streams/FileInputStream.swift
+++ streamer/Sources/Streams/FileInputStream.swift
@@ -26,7 +26,7 @@ open class FileInputStream: SeekableInputStream {
     /// The file handle (== fd) of the file at path `filePath`
     private var fileHandle: FileHandle?
 
-    /// <#Description#>
+    ///
     private var _streamError: Error?
     override open var streamError: Error? {
         get {
@@ -50,14 +50,14 @@ open class FileInputStream: SeekableInputStream {
         }
     }
 
-    /// <#Description#>
+    /// Current position in the stream.
     override public var offset: UInt64 {
         get {
             return fileHandle?.offsetInFile ?? 0
         }
     }
 
-    /// <#Description#>
+    /// True when the current offset is not arrived the the end of the stream.
     override open var hasBytesAvailable: Bool {
         get {
             return offset < _length
@@ -66,20 +66,25 @@ open class FileInputStream: SeekableInputStream {
 
     // MARK: - Public methods.
 
+    /// Initialize the object and the input stream meta data for file at 
+    /// `fileAtPath`.
     public init?(fileAtPath: String) {
         let attributes: [FileAttributeKey : Any]
 
+        // Does the file `atFilePath` exists
         guard FileManager.default.fileExists(atPath: fileAtPath) else {
             NSLog("File not found: \(fileAtPath)")
             return nil
         }
         filePath = fileAtPath
+        // Try to retrieve attributes of `fileAtPath`
         do {
             attributes = try FileManager.default.attributesOfItem(atPath: filePath)
         } catch {
             NSLog("Exception retrieving attrs for item at path \(filePath) (\(error)) ")
             return nil
         }
+        // Verify the size attribute of the file at `fileAtPath`
         guard let fileSize = attributes[FileAttributeKey.size] as? UInt64 else {
             NSLog("Error accessing size attribute")
             return nil
@@ -90,13 +95,13 @@ open class FileInputStream: SeekableInputStream {
 
     // MARK: - Open methods.
 
-    /// Open a file handle (fd) for file at path `filePath`.
+    /// Open a file handle (<=>fd) for file at path `filePath`.
     override open func open() {
         fileHandle = FileHandle(forReadingAtPath: filePath)
         _streamStatus = .open
     }
 
-    /// <#Description#>
+    /// Close the file handle.
     override open func close() {
         guard let fileHandle = fileHandle else {
             return
@@ -105,12 +110,7 @@ open class FileInputStream: SeekableInputStream {
         _streamStatus = .closed
     }
 
-    /// <#Description#>
-    ///
-    /// - Parameters:
-    ///   - buffer: <#buffer description#>
-    ///   - len: <#len description#>
-    /// - Returns: <#return value description#>
+    // TODO: to implement or delete ?
     override open func getBuffer(_ buffer: UnsafeMutablePointer<UnsafeMutablePointer<UInt8>?>,
                                  length len: UnsafeMutablePointer<Int>) -> Bool {
         return false
@@ -139,11 +139,11 @@ open class FileInputStream: SeekableInputStream {
         return Int(data.count)
     }
 
-    /// <#Description#>
+    /// Moves the file pointer to the specified offset within the file.
     ///
     /// - Parameters:
-    ///   - offset: <#offset description#>
-    ///   - whence: <#whence description#>
+    ///   - offset: The offset.
+    ///   - whence: From which position.
     override public func seek(offset: Int64, whence: SeekWhence) {
         assert(whence == .startOfFile, "Only seek from start of stream is supported for now.")
         assert(offset >= 0, "Since only seek from start of stream if supported, offset must be >= 0")
