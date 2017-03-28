386cf74e677f3123dce3a3a2442e2d5984659f47
Alexandre Camilleri <alexandre.camilleri@edrlab.com>
2017-03-28 14:49:00 +0200

    Reflect the Epub3.1 spec/WebPub Spec
    Former-commit-id: a2bb34ac270646ad0daca0bed9b4b2b0f70c2c88

--- streamer/Sources/model/Subject.swift
+++ streamer/Sources/model/Subject.swift
@@ -9,13 +9,19 @@
 import Foundation
 import ObjectMapper
 
-/// <#Description#>
-open class Subject: Mappable {
+/// WebPub manifest spec
+/// https://github.com/readium/webpub-manifest/blob/master/contexts/default/definitions.md#subjects
+/// Epub 3.1
+/// http://www.idpf.org/epub/31/spec/epub-packages.html#sec-opf-dcsubject
+public class Subject: Mappable {
 
-    public var name: String?
-    public var sortAs: String?
-    public var scheme: String?
-    public var code: String?
+    var name: String?
+    /// The WebPubManifest elements
+    var sortAs: String?
+    /// Epub 3.1 "scheme" (opf:authority)
+    var scheme: String?
+    /// Epub 3.1 "code" (opf:term)
+    var code: String?
 
     public init() {}
 
