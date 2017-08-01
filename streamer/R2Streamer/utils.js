0d9ee3916219b873cfd38a6130fe3c54347e5a3d
Alexandre Camilleri <alexandre.camilleri@edrlab.com>
2017-08-01 16:22:50 +0200

    New scollToId() js func
    Former-commit-id: e62d041ddae7bad5d3198b23bda4c60a2cb5dddb

--- streamer/R2Streamer/utils.js
+++ streamer/R2Streamer/utils.js
@@ -3,3 +3,13 @@
 //                        // Notify native code that the page is loaded.
 //                        webkit.messageHandlers.didLoad.postMessage("");
 //                        }, false);
+
+
+var scrollToId = function(id, screenWidth) {
+    var element = document.getElementById(id)
+    var rect = element.getBoundingClientRect();
+    var offset = window.scrollX + rect.left;
+    var snappedOffset = offset - (offset % screenWidth);
+
+    document.body.scrollLeft = snappedOffset;
+};
