6496101f1c685ccca4f40845e2116dfac9e5addd
Alexandre Camilleri <alexandre.camilleri@edrlab.com>
2017-09-11 11:37:50 +0200

    Model update
    Former-commit-id: b3ce5077c309a86e89b87e9adcfcda22a4a1d5ed

---
+++ lcp/readium-lcp-swift/Rights.swift
@@ -0,0 +1,38 @@
+//
+//  Right.swift
+//  readium-lcp-swift
+//
+//  Created by Alexandre Camilleri on 9/11/17.
+//  Copyright © 2017 Readium. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public struct Rights {
+    /// Maximum number of pages that can be printed over the lifetime of the
+    /// license.
+    var print: Int?
+    /// Maximum number of characters that can be copied to the clipboard
+    /// over the lifetime of the license.
+    var copy: Int?
+    /// Date and time when the license begins.
+    var start: Date?
+    /// Date and time when the license ends.
+    var end: Date?
+
+    init(with json: JSON) {
+        print = json["print"].int
+        copy = json["copy"].int
+        if let start = json["start"].string,
+            let startDate = start.dateFromISO8601
+        {
+            self.start = startDate
+        }
+        if let end =  json["end"].string,
+            let endDate = end.dateFromISO8601
+        {
+            self.end = endDate
+        }
+    }
+}
---
+++ lcp/readium-lcp-swift/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  readium-lcp-swift
+//
+//  Created by Alexandre Camilleri on 9/11/17.
+//  Copyright © 2017 Readium. All rights reserved.
+//
+
+import Foundation
