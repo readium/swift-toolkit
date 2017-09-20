915c5fe18f571b19d619782c3b456d7dad3358a0
Alexandre Camilleri <alexandre.camilleri@edrlab.com>
2017-09-20 14:24:05 +0200

    Updated date extension

--- shared/r2-shared-swift/ISO8601DateString.swift
+++ shared/r2-shared-swift/ISO8601DateString.swift
@@ -23,7 +23,7 @@ extension Formatter {
         formatter.calendar = Calendar(identifier: .iso8601)
         formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
         return formatter
     }()
 }
