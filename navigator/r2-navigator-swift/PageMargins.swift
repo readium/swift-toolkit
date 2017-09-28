a67aed1b81e700cf949bbf295fa047321f64916f
Alexandre Camilleri <alexandre.camilleri@edrlab.com>
2017-09-28 16:55:14 +0200

    Problem with default value of margins fixed

--- navigator/r2-navigator-swift/PageMargins.swift
+++ navigator/r2-navigator-swift/PageMargins.swift
@@ -14,30 +14,31 @@ extension UserSettings {
         public let min = 0.5
         public let max = 2.0
         public var value: Double!
-
+        
         public init(initialValue: Double) {
-            if initialValue < min || initialValue > max,
+            if (initialValue < min || initialValue > max) ||
                 (initialValue.truncatingRemainder(dividingBy: step) != 0)
             {
-                value = 1
+                value = 1.0
+            } else {
+                value = initialValue
             }
-            value = initialValue
         }
-
+        
         public func increment() {
             guard value + step <= max else {
                 return
             }
             value = value + step
         }
-
+        
         public func decrement() {
             guard value - step >= min else {
                 return
             }
             value = value - step
         }
-
+        
         public func stringValue() -> String {
             return "\(value!)"
         }
