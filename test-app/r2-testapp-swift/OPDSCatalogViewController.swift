7aa03d2b8a3418aa414f6bea686073f41da7ede8
Alexandre Camilleri <alexandre.camilleri@edrlab.org>
2017-10-30 11:59:13 +0100

    Error whitespace
    Former-commit-id: c31c22fb98ce08ebed113fe0aca9d1e98c364c50

---
+++ test-app/r2-testapp-swift/OPDSCatalogViewController.swift
@@ -0,0 +1,14 @@
+//
+//  OPDSLibraryViewController.swift
+//  r2-testapp-swift
+//
+//  Created by Alexandre Camilleri on 10/30/17.
+//  Copyright © 2017 Readium. All rights reserved.
+//
+
+import UIKit
+import ReadiumOPDS
+
+class OPDSLibraryViewController: UICollectionViewController {
+    
+}
