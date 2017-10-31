2d410c3dc7a15f4110a9744d4a1b22ca6403878b
Alexandre Camilleri <alexandre.camilleri@edrlab.org>
2017-10-31 10:48:04 +0100

    adding r2lcpclient
    Former-commit-id: eda909f98bc929863173ce11894f8856370f37e7

---
+++ lcp/R2LCPClient.framework/Headers/r2_lcp_client_swift_wrapper.h
@@ -0,0 +1,19 @@
+//
+//  r2_lcp_client_swift_wrapper.h
+//  r2-lcp-client-swift-wrapper
+//
+//  Created by Alexandre Camilleri on 10/13/17.
+//  Copyright © 2017 Readium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for r2_lcp_client_swift_wrapper.
+FOUNDATION_EXPORT double r2_lcp_client_swift_wrapperVersionNumber;
+
+//! Project version string for r2_lcp_client_swift_wrapper.
+FOUNDATION_EXPORT const unsigned char r2_lcp_client_swift_wrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <r2_lcp_client_swift_wrapper/PublicHeader.h>
+
+
