ba9d987dfa8e589e6a0fd8c197418f93f0c2b996
Geoffrey Bugniot <g.bugniot@gmail.com>
2018-04-06 15:20:18 +0200

    Fix a crash occuring when a book is added to the library
    Former-commit-id: 11515833a3e10e1eb56cc3b281507b1673ae72c3

--- test-app/r2-testapp-swift/AppDelegate.swift
+++ test-app/r2-testapp-swift/AppDelegate.swift
@@ -85,9 +85,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     /// Called when the user open a file outside of the application and open it
     /// with the application.
-    func application(_ application: UIApplication, open url: URL,
-                     sourceApplication: String?, annotation: Any) -> Bool
-    {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
         guard url.isFileURL else {
             showInfoAlert(title: "Error", message: "The document isn't valid.")
             return false
@@ -103,7 +101,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     if self.lightParsePublication(at: Location(absolutePath: publicationUrl.path,
                                                                relativePath: "",
                                                                type: .epub)) {
-
                         self.showInfoAlert(title: "Success", message: "LCP Publication added to library.")
                         self.reload()
                     } else {
