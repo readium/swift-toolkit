314f56c9bcc734138f7cb1bab934be5145d49998
iaomw <iaomw@live.com>
2018-06-08 16:08:50 +0200

    Move userVersion of Connection to the file of LCPDatabase
    Former-commit-id: e59ab9bdc1fb6322384ba851c96371b3c85cb03a

--- lcp/readium-lcp-swift/Licenses.swift
+++ lcp/readium-lcp-swift/Licenses.swift
@@ -144,10 +144,3 @@ class Licenses {
     }
     
 }
-
-extension Connection {
-    public var userVersion: Int32 {
-        get { return Int32(try! scalar("PRAGMA user_version") as! Int64)}
-        set { try! run("PRAGMA user_version = \(newValue)") }
-    }
-}
