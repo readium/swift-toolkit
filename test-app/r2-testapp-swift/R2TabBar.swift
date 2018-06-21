5847639a347781bd7f72705b9e481706a9eb0040
iaomw <iaomw@live.com>
2018-06-21 17:21:08 +0200

    Fix tabbar for iPhone X.
    Former-commit-id: e4845a201b580860d089f9e0a65a0f3b67fff401

---
+++ test-app/r2-testapp-swift/R2TabBar.swift
@@ -0,0 +1,53 @@
+//
+//  R2TabBar.swift
+//  r2-testapp-swift
+//
+//  Created by Senda Li on 2018/6/21.
+//  Copyright © 2018 Readium. All rights reserved.
+//  https://stackoverflow.com/questions/46214740/ios-11-iphone-x-simulator-uitabbar-icons-and-titles-being-rendered-on-top-coveri
+
+import UIKit
+
+class R2TabBar: UITabBar {
+    
+    var oldSafeAreaInsets = UIEdgeInsets.zero
+    
+    @available(iOS 11.0, *)
+    override func safeAreaInsetsDidChange() {
+        super.safeAreaInsetsDidChange()
+        
+        if oldSafeAreaInsets != safeAreaInsets {
+            oldSafeAreaInsets = safeAreaInsets
+            
+            invalidateIntrinsicContentSize()
+            superview?.setNeedsLayout()
+            superview?.layoutSubviews()
+        }
+    }
+    
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        var size = super.sizeThatFits(size)
+        if #available(iOS 11.0, *) {
+            let bottomInset = safeAreaInsets.bottom
+            if bottomInset > 0 && size.height < 50 && (size.height + bottomInset < 90) {
+                size.height += bottomInset
+            }
+        }
+        return size
+    }
+    
+    override var frame: CGRect {
+        get {
+            return super.frame
+        }
+        set {
+            var tmp = newValue
+            if let superview = superview, tmp.maxY !=
+                superview.frame.height {
+                tmp.origin.y = superview.frame.height - tmp.height
+            }
+            
+            super.frame = tmp
+        }
+    }
+}
