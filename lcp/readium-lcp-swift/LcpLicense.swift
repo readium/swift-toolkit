b8c01c80f078d443547f76fdbab738b2ea675985
Geoffrey Bugniot <g.bugniot@gmail.com>
2018-07-09 15:23:11 +0200

    Add copyright headers and license file
    Former-commit-id: b129493dc3269038bfa071524e335764ac11d232

--- lcp/readium-lcp-swift/LcpLicense.swift
+++ lcp/readium-lcp-swift/LcpLicense.swift
@@ -3,7 +3,10 @@
 //  readium-lcp-swift
 //
 //  Created by Alexandre Camilleri on 9/14/17.
-//  Copyright © 2017 Readium. All rights reserved.
+//
+//  Copyright 2018 Readium Foundation. All rights reserved.
+//  Use of this source code is governed by a BSD-style license which is detailed
+//  in the LICENSE file present in the project repository where this source code is maintained.
 //
 
 import Foundation
