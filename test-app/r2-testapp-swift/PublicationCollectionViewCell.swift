cd445ec46e06f86106db5cffe574b797434637e4
Geoffrey Bugniot <g.bugniot@gmail.com>
2018-07-26 10:43:18 +0200

    Add a specific type of view to be used as a publication cover across the app
    Former-commit-id: 3c4662bda6a77b9435fffcf1a6a47bf97295b056

---
+++ test-app/r2-testapp-swift/PublicationCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PublicationCollectionViewCell.swift
+//  r2-testapp-swift
+//
+//  Created by Geoffrey Bugniot on 26/07/2018.
+//  Copyright © 2018 Readium. All rights reserved.
+//
+
+import UIKit
+
+class PublicationCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
