9837f9239b8e643bf01fc471258a8d1dcf5c0055
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2019-02-05 16:21:28 -0500

    fixed copyright
    Former-commit-id: a27f9924210c744776e6dc6cebd80aa48c871428

--- lcp/readium-lcp-swift/Result.swift
+++ lcp/readium-lcp-swift/Result.swift
@@ -1,6 +1,12 @@
 //
+//  Result.swift
+//  r2-lcp-swift
+//
 //  Created by Mickaël Menu on 04.02.19.
-//  Copyright © 2019 Readium. All rights reserved.
+//
+//  Copyright 2018 Readium Foundation. All rights reserved.
+//  Use of this source code is governed by a BSD-style license which is detailed
+//  in the LICENSE file present in the project repository where this source code is maintained.
 //
 
 import Foundation
