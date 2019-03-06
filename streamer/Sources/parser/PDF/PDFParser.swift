229e58dc6b3edebaef7d6a8187944f13376ff7d2
Mickaël Menu <mmenu@mantano.com>
2019-03-06 16:35:43 +0100

    Parse standalone PDF metadata using CGPDFDocument
    Former-commit-id: d971639297aff0ba4d0339ef5263167f45b7bd20

--- streamer/Sources/parser/PDF/PDFParser.swift
+++ streamer/Sources/parser/PDF/PDFParser.swift
@@ -10,6 +10,7 @@
 //
 
 import Foundation
+import CoreGraphics
 import R2Shared
 
 
@@ -17,7 +18,7 @@ import R2Shared
 public struct PDFConstant {
     /// PDF mime-type.
     public static let mimetype = "application/pdf"
-    /// PDF file path inside the container.
+    /// Default PDF file path inside the container, for standalone PDF files.
     public static let pdfFilePath = "/publication.pdf"
 }
 
@@ -26,18 +27,26 @@ public struct PDFConstant {
 public enum PDFParserError: LocalizedError {
     // The file at 'path' is missing from the container.
     case missingFile(path: String)
+    // Failed to open the PDF
+    case openFailed
+    // The PDF is encrypted with a password. This is not supported right now.
+    case fileEncryptedWithPassword
     
     public var errorDescription: String? {
         switch self {
         case .missingFile(let path):
             return "The file '\(path)' is missing."
+        case .openFailed:
+            return "Can't open the PDF file."
+        case .fileEncryptedWithPassword:
+            return "The PDF is encrypted with a password."
         }
     }
     
 }
 
 
-public final class PDFParser: PublicationParser {
+public final class PDFParser: PublicationParser, Loggable {
 
     /// PDFParser contains only static methods.
     private init() {}
@@ -49,29 +58,28 @@ public final class PDFParser: PublicationParser {
     /// - Throws: `PDFParserError`
     public static func parse(fileAtPath path: String) throws -> (PubBox, PubParsingCallback) {
         let container = try generateContainerFrom(fileAtPath: path)
-        
+
         let publication = Publication()
         publication.updatedDate = container.modificationDate
         publication.internalData["type"] = "pdf"
         publication.internalData["rootfile"] = container.rootFile.rootFilePath
-        
-        // FIXME: pull metadata from the PDF itself
-        publication.metadata.multilangTitle = title(from: path)
-        publication.metadata.identifier = path
-        
+
         let link = Link()
         link.typeLink = PDFConstant.mimetype
         link.href = PDFConstant.pdfFilePath
         publication.readingOrder.append(link)
         
-        func didLoadDRM(drm: DRM?){
+        // FIXME: if the PDF is DRM-protected, this will not work here
+        try self.fillMetadata(of: publication, in: container)
+        
+        func didLoadDRM(drm: DRM?) throws {
             container.drm = drm
         }
         
         return ((publication, container), didLoadDRM)
     }
 
-    static private func generateContainerFrom(fileAtPath path: String) throws -> Container {
+    private static func generateContainerFrom(fileAtPath path: String) throws -> Container {
         var isDirectory: ObjCBool = false
         guard FileManager.default.fileExists(atPath: path, isDirectory: &isDirectory) else {
             throw PDFParserError.missingFile(path: path)
@@ -81,7 +89,6 @@ public final class PDFParser: PublicationParser {
         if isDirectory.boolValue {
             container = DirectoryContainer(directory: path, mimetype: PDFConstant.mimetype)
         } else {
-            // FIXME: Which relative path to use here? It depends on how the fetcher access it
             container = FileContainer(path: path, relativePath: PDFConstant.pdfFilePath, mimetype: PDFConstant.mimetype)
         }
         
@@ -91,13 +98,34 @@ public final class PDFParser: PublicationParser {
         return containerUnwrapped
     }
     
-    static private func title(from path: String) -> MultilangString {
-        let title = URL(fileURLWithPath: path)
-            .deletingPathExtension()
-            .lastPathComponent
-            .replacingOccurrences(of: "_", with: " ")
+    /// Extracts the metadata of a PDF file to fill the `Publication`.
+    private static func fillMetadata(of publication: Publication, in container: Container) throws {
+        /// The PDF might be encrypted, so we filter it with the fetcher.
+        guard let fetcher = try? Fetcher(publication: publication, container: container),
+            let link = publication.readingOrder.first,
+            let optionalStream = try? fetcher.dataStream(forLink: link),
+            let stream = optionalStream else
+        {
+            throw PDFParserError.openFailed
+        }
         
-        return MultilangString(single: title)
+        let metadata = try PDFFileMetadata.parse(from: stream)
+
+        publication.version = metadata.version
+        publication.metadata.identifier = metadata.identifier ?? container.rootFile.rootPath
+
+        if let authorName = metadata.author {
+            let author = Contributor()
+            author.multilangName = MultilangString(single: authorName)
+            publication.metadata.authors.append(author)
+        }
+
+        let title = metadata.title
+            ?? URL(fileURLWithPath: container.rootFile.rootPath)
+                .deletingPathExtension()
+                .lastPathComponent
+                .replacingOccurrences(of: "_", with: " ")
+        publication.metadata.multilangTitle = MultilangString(single: title)
     }
 
 }
