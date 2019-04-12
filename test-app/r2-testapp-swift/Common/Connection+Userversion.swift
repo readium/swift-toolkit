76e168d89720851773be742869bb8993544174f9
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2019-04-12 15:54:42 -0400

    fix copyright
    Former-commit-id: c78b7ca2bcffc4e11814bf547155af056dd31732

--- test-app/r2-testapp-swift/Common/Connection+Userversion.swift
+++ test-app/r2-testapp-swift/Common/Connection+Userversion.swift
@@ -3,7 +3,11 @@
 //  r2-testapp-swift
 //
 //  Created by Aferdita Muriqi on 4/7/19.
-//  Copyright © 2019 Readium. All rights reserved.
+//
+//  Copyright 2018 European Digital Reading Lab. All rights reserved.
+//  Licensed to the Readium Foundation under one or more contributor license agreements.
+//  Use of this source code is governed by a BSD-style license which is detailed in the
+//  LICENSE file present in the project repository where this source code is maintained.
 //
 
 import Foundation
