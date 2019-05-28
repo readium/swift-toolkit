cfb5ad3e424754ecb5a9f45934bce2f65c6afffd
Mickaël Menu <mmenu@mantano.com>
2019-05-28 12:15:56 +0200

    Reorganize file structure and fix case issues
    Former-commit-id: 54585b445fcae4d17627feb408dfd5be372fbd7e

---
+++ streamer/r2-streamer-swift/r2-streamer-swift-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  r2-streamer-swift-Bridging-Header.h
+//  r2-streamer-swift
+//
+//  Created by Olivier Körner on 21/12/2016.
+//
+//  Copyright 2018 Readium Foundation. All rights reserved.
+//  Use of this source code is governed by a BSD-style license which is detailed
+//  in the LICENSE file present in the project repository where this source code is maintained.
+//
+
+#ifndef R2Streamer_Bridging_Header_h
+#define R2Streamer_Bridging_Header_h
+
+#import <GCDWebServer/GCDWebServer.h>
+#import <GCDWebServer/GCDWebServerDataResponse.h>
+
+#endif /* R2Streamer_Bridging_Header_h */
