05874e90fba6e42e7f0564517066c3cc07d26bc9
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2022-05-17 00:02:34 -0500

    Add license info to new files, clean up About tab contents

--- TestApp/Sources/About/AboutTab.swift
+++ TestApp/Sources/About/AboutTab.swift
@@ -4,24 +4,38 @@
 //
 //  Created by Steven Zeck on 5/15/22.
 //
+//  Copyright 2022 European Digital Reading Lab. All rights reserved.
+//  Licensed to the Readium Foundation under one or more contributor license agreements.
+//  Use of this source code is governed by a BSD-style license which is detailed in the
+//  LICENSE file present in the project repository where this source code is maintained.
+//
 
 import SwiftUI
 
 struct AboutTab: View {
     var body: some View {
-        VStack {
-            HStack {
+        NavigationView {
+            VStack(alignment: .leading, spacing: 20) {
                 Text("Version")
-                Text("2.3.0")
+                    .font(.title2)
+                HStack(spacing: 10) {
+                    Text("Version").frame(width: 170.0, alignment: .leading)
+                    Text("2.3.0")
+                }
+                HStack(spacing: 10) {
+                    Text("Build").frame(width: 170.0, alignment: .leading)
+                    Text("1")
+                }
+                Text("Copyright").font(.title2)
+                Link("© 2022 European Digital Reading Lab",
+                     destination: URL(string: "https://www.edrlab.org/")!)
+                Link("[BSD-3 License]",
+                     destination: URL(string: "https://opensource.org/licenses/BSD-3-Clause")!)
+                Text("Acknowledgements").font(.title2)
+                Text("R2 Reader wouldn't have been developed without the financial help of the French State.")
+                Image("rf")
             }
-            HStack {
-                Text("Build")
-                Text("1")
-            }
-            Text("© 2022 European Digital Reading Lab")
-            Text("[BSD-3 License]")
-            Text("R2 Reader wouldn't have been developed without the financial help of the French State.")
-            Image("rf")
+            .navigationTitle("About")
         }
     }
 }
--- TestApp/Sources/ContentView.swift
+++ TestApp/Sources/ContentView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Steven Zeck on 5/15/22.
 //
-//
 //  Copyright 2022 European Digital Reading Lab. All rights reserved.
 //  Licensed to the Readium Foundation under one or more contributor license agreements.
 //  Use of this source code is governed by a BSD-style license which is detailed in the
