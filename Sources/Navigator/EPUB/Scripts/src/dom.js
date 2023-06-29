1c7d36e8cf8af68d615891bad6f607c638a2e813
Mickaël Menu <mickael.menu@gmail.com>
2023-06-29 16:33:07 +0200

    Improve HTML `firstVisibleElementLocator`

--- Sources/Navigator/EPUB/Scripts/src/dom.js
+++ Sources/Navigator/EPUB/Scripts/src/dom.js
@@ -54,10 +54,6 @@ export function findNearestInteractiveElement(element) {
 /// the screen.
 export function findFirstVisibleLocator() {
   const element = findElement(document.body);
-  if (!element) {
-    return undefined;
-  }
-
   return {
     href: "#",
     type: "application/xhtml+xml",
@@ -71,52 +67,30 @@ export function findFirstVisibleLocator() {
 }
 
 function findElement(rootElement) {
-  var foundElement = undefined;
-  for (var i = rootElement.children.length - 1; i >= 0; i--) {
+  for (var i = 0; i < rootElement.children.length; i++) {
     const child = rootElement.children[i];
-    const position = elementRelativePosition(child, undefined);
-    if (position == 0) {
-      if (!shouldIgnoreElement(child)) {
-        foundElement = child;
-      }
-    } else if (position < 0) {
-      if (!foundElement) {
-        foundElement = child;
-      }
-      break;
+    if (!shouldIgnoreElement(child) && isElementVisible(child)) {
+      return findElement(child);
     }
-  }
-
-  if (foundElement) {
-    return findElement(foundElement);
   }
   return rootElement;
 }
 
-// See computeVisibility_() in r2-navigator-js
-function elementRelativePosition(element, domRect /* nullable */) {
+function isElementVisible(element) {
   if (readium.isFixedLayout) return true;
 
   if (element === document.body || element === document.documentElement) {
-    return -1;
+    return true;
   }
   if (!document || !document.documentElement || !document.body) {
-    return 1;
+    return false;
   }
 
-  const rect = domRect || element.getBoundingClientRect();
-
+  const rect = element.getBoundingClientRect();
   if (isScrollModeEnabled()) {
-    return rect.top >= 0 && rect.top <= document.documentElement.clientHeight;
+    return rect.bottom > 0 && rect.top < window.innerHeight;
   } else {
-    const pageWidth = window.innerWidth;
-    if (rect.left >= pageWidth) {
-      return 1;
-    } else if (rect.left >= 0) {
-      return 0;
-    } else {
-      return -1;
-    }
+    return rect.right > 0 && rect.left < window.innerWidth;
   }
 }
 
@@ -124,7 +98,7 @@ function shouldIgnoreElement(element) {
   const elStyle = getComputedStyle(element);
   if (elStyle) {
     const display = elStyle.getPropertyValue("display");
-    if (display === "none") {
+    if (display != "block") {
       return true;
     }
     // Cannot be relied upon, because web browser engine reports invisible when out of view in
