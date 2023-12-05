cf1022b876a836e21d936e83bfa67a0730e6fe7b
chrfalch <christian.falch@gmail.com>
2023-12-05 17:11:50 +0100

    Fix EPUBNavigatorViewController transition logic
    To be able to handle rotation and screen changes which causes a full spread reload we need to keep the locator in the transition change.
    
    Fixes #367

--- Sources/Navigator/EPUB/EPUBNavigatorViewController.swift
+++ Sources/Navigator/EPUB/EPUBNavigatorViewController.swift
@@ -166,9 +166,12 @@ open class EPUBNavigatorViewController: UIViewController,
 
         mutating func transition(_ event: Event) -> Bool {
             switch (self, event) {
+            // Loading the spreads is always possible, because it can be triggered by rotating the
+            // screen. In which case it cancels any on-going state.
+            case let (_, .load(locator)):
+                self = .loading(pendingLocator: locator)
+
             // All events are ignored when loading spreads, except for `loaded` and `load`.
-            case (.loading, .load):
-                return true
             case (.loading, .loaded):
                 self = .idle
             case (.loading, _):
@@ -192,11 +195,6 @@ open class EPUBNavigatorViewController: UIViewController,
             case (.moving, .jump),
                  (.moving, .move):
                 return false
-
-            // Loading the spreads is always possible, because it can be triggered by rotating the
-            // screen. In which case it cancels any on-going state.
-            case let (_, .load(locator)):
-                self = .loading(pendingLocator: locator)
 
             default:
                 log(.error, "Invalid event \(event) for state \(self)")
