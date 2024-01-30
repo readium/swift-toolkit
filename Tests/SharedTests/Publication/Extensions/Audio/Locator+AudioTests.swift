3ebca170a9b64902eca2d429f4c4ad60ab205afc
Mickaël Menu <mickael.menu@gmail.com>
2024-01-30 13:22:49 +0100

    Lint

--- Tests/SharedTests/Publication/Extensions/Audio/Locator+AudioTests.swift
+++ Tests/SharedTests/Publication/Extensions/Audio/Locator+AudioTests.swift
@@ -8,36 +8,36 @@
 import XCTest
 
 class LocatorLocationsAudioTests: XCTestCase {
-  func testNoFragment() {
-    XCTAssertNil(Locator.Locations().time)
-  }
-
-  func testMalformedFragment() {
-    XCTAssertNil(Locator.Locations(fragments: ["t=one"]).time)
-  }
-
-  func testValidFragments() {
-    continueAfterFailure = false
-    for beginStr in ["", "1", "1.0", "1.1"] {
-      for endStr in ["", ",", ",1", ",1.0", ",1.1"] {
-        let val = beginStr + endStr
-        if val == "" || val == "," {
-          continue
-        }
-        let locations = Locator.Locations(fragments: ["t=\(val)"])
-        let time = locations.time
-        switch time {
-        case let .begin(begin):
-          XCTAssertEqual(begin, Double(beginStr))
-        case let .end(end):
-          XCTAssertEqual(end, Double(endStr.replacingPrefix(",", by: "")))
-        case let .interval(begin, end):
-          XCTAssertEqual(begin, Double(beginStr))
-          XCTAssertEqual(end, Double(endStr.replacingPrefix(",", by: "")))
-        case nil:
-          XCTAssertNotNil(time)
-        }
-      }
+    func testNoFragment() {
+        XCTAssertNil(Locator.Locations().time)
     }
-  }
+
+    func testMalformedFragment() {
+        XCTAssertNil(Locator.Locations(fragments: ["t=one"]).time)
+    }
+
+    func testValidFragments() {
+        continueAfterFailure = false
+        for beginStr in ["", "1", "1.0", "1.1"] {
+            for endStr in ["", ",", ",1", ",1.0", ",1.1"] {
+                let val = beginStr + endStr
+                if val == "" || val == "," {
+                    continue
+                }
+                let locations = Locator.Locations(fragments: ["t=\(val)"])
+                let time = locations.time
+                switch time {
+                case let .begin(begin):
+                    XCTAssertEqual(begin, Double(beginStr))
+                case let .end(end):
+                    XCTAssertEqual(end, Double(endStr.replacingPrefix(",", by: "")))
+                case let .interval(begin, end):
+                    XCTAssertEqual(begin, Double(beginStr))
+                    XCTAssertEqual(end, Double(endStr.replacingPrefix(",", by: "")))
+                case nil:
+                    XCTAssertNotNil(time)
+                }
+            }
+        }
+    }
 }
