2a4ce7c84842587113e1924544db66af65bbdf93
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2024-03-31 20:39:55 -0500

    Cleanup

--- TestApp/Sources/Data/Bookmark.swift
+++ TestApp/Sources/Data/Bookmark.swift
@@ -53,15 +53,16 @@ final class BookmarkRepository {
         }
     }
     
-    func add(_ bookmark: Bookmark) -> AnyPublisher<Bookmark.Id, Error> {
-        return db.writePublisher { db in
+    @discardableResult
+    func add(_ bookmark: Bookmark) async throws -> Bookmark.Id {
+        try await db.write { db in
             try bookmark.insert(db)
             return Bookmark.Id(rawValue: db.lastInsertedRowID)
-        }.eraseToAnyPublisher()
+        }
     }
-    
-    func remove(_ id: Bookmark.Id) -> AnyPublisher<Void, Error> {
-        db.writePublisher { db in try Bookmark.deleteOne(db, key: id) }
+
+    func remove(_ id: Bookmark.Id) async throws {
+        try await db.write { db in try Bookmark.deleteOne(db, key: id) }
     }
 
 }
