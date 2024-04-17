364d2cf1797c6dd147388e5e95981548a579b82e
Dom Kiva Meyer <dom@domkm.com>
2024-04-17 13:30:57 -0700

    Add resource metadata extraction to AudioParser

--- Sources/Streamer/Parser/Audio/AudioParser.swift
+++ Sources/Streamer/Parser/Audio/AudioParser.swift
@@ -4,6 +4,7 @@
 //  available in the top-level LICENSE file of the project.
 //
 
+import AVFoundation
 import Foundation
 import R2Shared
 
@@ -14,12 +15,16 @@ import R2Shared
 public final class AudioParser: PublicationParser {
     public init() {}
 
+    private func metadataItems(_ metadataItems: [AVMetadataItem], _ key: AVMetadataKey) -> [AVMetadataItem] {
+        AVMetadataItem.metadataItems(from: metadataItems, withKey: key, keySpace: .common)
+    }
+
     public func parse(asset: PublicationAsset, fetcher: Fetcher, warnings: WarningLogger?) throws -> Publication.Builder? {
         guard accepts(asset, fetcher) else {
             return nil
         }
 
-        let readingOrder = fetcher.links
+        var readingOrder = fetcher.links
             .filter { !ignores($0) && $0.mediaType.isAudio }
             .sorted { $0.href.localizedCaseInsensitiveCompare($1.href) == .orderedAscending }
 
@@ -27,14 +32,44 @@ public final class AudioParser: PublicationParser {
             return nil
         }
 
+        let avAssets = readingOrder.map { link in fetcher.get(link).file.map { AVURLAsset(url: $0, options: [AVURLAssetPreferPreciseDurationAndTimingKey: true]) } }
+
+        readingOrder = zip(readingOrder, avAssets).map { link, avAsset in
+            guard let avAsset else { return link }
+            return link.copy(
+                title: metadataItems(avAsset.metadata, .commonKeyTitle).first?.stringValue,
+                bitrate: avAsset.tracks(withMediaType: .audio).first.map { Double($0.estimatedDataRate) },
+                duration: avAsset.duration.seconds
+            )
+        }
+
+        let durations = readingOrder.compactMap(\.duration)
+
+        var metadata = Metadata(
+            conformsTo: [.audiobook],
+            title: fetcher.guessTitle(ignoring: ignores) ?? asset.name,
+            duration: readingOrder.count == durations.count ? durations.reduce(0, +) : nil
+        )
+
+        if let avMetadata = avAssets.compactMap({ $0 }).first?.metadata {
+            metadata = metadata.copy(
+                title: metadataItems(avMetadata, .commonKeyTitle).first?.stringValue ?? metadata.title,
+                modified: metadataItems(avMetadata, .commonKeyLastModifiedDate).first?.stringValue?.dateFromISO8601,
+                published: metadataItems(avMetadata, .commonKeyCreationDate).first?.stringValue?.dateFromISO8601,
+                languages: metadataItems(avMetadata, .commonKeyLanguage).compactMap(\.stringValue),
+                subjects: metadataItems(avMetadata, .commonKeySubject).compactMap(\.stringValue).map { Subject(name: $0) },
+                authors: metadataItems(avMetadata, .commonKeyAuthor).compactMap(\.stringValue).map { Contributor(name: $0) },
+                contributors: metadataItems(avMetadata, .commonKeyContributor).compactMap(\.stringValue).map { Contributor(name: $0) },
+                publishers: metadataItems(avMetadata, .commonKeyPublisher).compactMap(\.stringValue).map { Contributor(name: $0) },
+                description: metadataItems(avMetadata, .commonKeyDescription).first?.stringValue
+            )
+        }
+
         return Publication.Builder(
             mediaType: .zab,
             format: .cbz,
             manifest: Manifest(
-                metadata: Metadata(
-                    conformsTo: [.audiobook],
-                    title: fetcher.guessTitle(ignoring: ignores) ?? asset.name
-                ),
+                metadata: metadata,
                 readingOrder: readingOrder
             ),
             fetcher: fetcher,
