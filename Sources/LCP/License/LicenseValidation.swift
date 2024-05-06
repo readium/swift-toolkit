390e1d0d915a658f3787c6ea91bb2b2352d1cd95
Mickaël Menu <mickael.menu@gmail.com>
2024-05-06 13:19:58 +0200

    Support for the new 2.x LCP Profiles (#418)

--- Sources/LCP/License/LicenseValidation.swift
+++ Sources/LCP/License/LicenseValidation.swift
@@ -11,6 +11,16 @@ import R2Shared
 private let supportedProfiles = [
     "http://readium.org/lcp/basic-profile",
     "http://readium.org/lcp/profile-1.0",
+    "http://readium.org/lcp/profile-2.0",
+    "http://readium.org/lcp/profile-2.1",
+    "http://readium.org/lcp/profile-2.2",
+    "http://readium.org/lcp/profile-2.3",
+    "http://readium.org/lcp/profile-2.4",
+    "http://readium.org/lcp/profile-2.5",
+    "http://readium.org/lcp/profile-2.6",
+    "http://readium.org/lcp/profile-2.7",
+    "http://readium.org/lcp/profile-2.8",
+    "http://readium.org/lcp/profile-2.9",
 ]
 
 typealias Context = Either<LCPClientContext, StatusError>
