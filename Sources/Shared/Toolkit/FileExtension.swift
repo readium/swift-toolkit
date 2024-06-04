b2a68226399f516881f5d8b20636f663f20dba39
Mickaël Menu <mickael.menu@gmail.com>
2024-06-04 21:06:55 +0200

    Add `FileExtension`

---
+++ Sources/Shared/Toolkit/FileExtension.swift
@@ -0,0 +1,27 @@
+//
+//  Copyright 2024 Readium Foundation. All rights reserved.
+//  Use of this source code is governed by the BSD-style license
+//  available in the top-level LICENSE file of the project.
+//
+
+import Foundation
+
+/// Represents a file extension.
+public struct FileExtension: Hashable, RawRepresentable, ExpressibleByStringLiteral {
+    
+    public let rawValue: String
+
+    public init(rawValue: String) {
+        precondition(!rawValue.isEmpty && rawValue.hasPrefix("."))
+        self.rawValue = rawValue.lowercased()
+    }
+    
+    public init(stringLiteral value: StringLiteralType) {
+        self.init(rawValue: value)
+    }
+    
+    /// Appends this file extension to `filename`.
+    public func appendToFilename(_ filename: String) -> String {
+        "\(filename).\(rawValue)"
+    }
+}
