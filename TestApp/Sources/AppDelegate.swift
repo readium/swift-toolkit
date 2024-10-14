5461daf9f5f05bfd3aba1a064082768c530e6646
Kirill Shakhansky <k.shakhanskiy@uzum.com>
2024-10-14 19:00:34 +0300

    setup for debug

--- TestApp/Sources/AppDelegate.swift
+++ TestApp/Sources/AppDelegate.swift
@@ -16,38 +16,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     private var subscriptions = Set<AnyCancellable>()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        app = try! AppModule()
-
-        func makeItem(title: String, image: String) -> UITabBarItem {
-            UITabBarItem(
-                title: NSLocalizedString(title, comment: "Library tab title"),
-                image: UIImage(named: image),
-                tag: 0
-            )
-        }
-
-        // Library
-        let libraryViewController = app.library.rootViewController
-        libraryViewController.tabBarItem = makeItem(title: "bookshelf_tab", image: "bookshelf")
-
-        // OPDS Feeds
-        let opdsViewController = app.opds.rootViewController
-        opdsViewController.tabBarItem = makeItem(title: "catalogs_tab", image: "catalogs")
-
-        // About
-        let aboutViewController = app.aboutViewController
-        aboutViewController.tabBarItem = makeItem(title: "about_tab", image: "about")
-
-        let tabBarController = UITabBarController()
-        tabBarController.viewControllers = [
-            libraryViewController,
-            opdsViewController,
-            aboutViewController,
-        ]
-
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = tabBarController
-        window?.makeKeyAndVisible()
+//        app = try! AppModule()
+//
+//        func makeItem(title: String, image: String) -> UITabBarItem {
+//            UITabBarItem(
+//                title: NSLocalizedString(title, comment: "Library tab title"),
+//                image: UIImage(named: image),
+//                tag: 0
+//            )
+//        }
+//
+//        // Library
+//        let libraryViewController = app.library.rootViewController
+//        libraryViewController.tabBarItem = makeItem(title: "bookshelf_tab", image: "bookshelf")
+//
+//        // OPDS Feeds
+//        let opdsViewController = app.opds.rootViewController
+//        opdsViewController.tabBarItem = makeItem(title: "catalogs_tab", image: "catalogs")
+//
+//        // About
+//        let aboutViewController = app.aboutViewController
+//        aboutViewController.tabBarItem = makeItem(title: "about_tab", image: "about")
+//
+//        let tabBarController = UITabBarController()
+//        tabBarController.viewControllers = [
+//            libraryViewController,
+//            opdsViewController,
+//            aboutViewController,
+//        ]
+//
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        window?.rootViewController = tabBarController
+//        window?.makeKeyAndVisible()
 
         return true
     }
