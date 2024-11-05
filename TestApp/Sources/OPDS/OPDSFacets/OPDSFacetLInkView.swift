24a284dd0f01689bad4039a6eeea05ac39061f54
Kirill Shakhansky <bb4bbb@gmail.com>
2024-11-05 17:14:50 +0300

    add OPDSFacetLInkView

---
+++ TestApp/Sources/OPDS/OPDSFacets/OPDSFacetLInkView.swift
@@ -0,0 +1,40 @@
+//
+//  Copyright 2024 Readium Foundation. All rights reserved.
+//  Use of this source code is governed by the BSD-style license
+//  available in the top-level LICENSE file of the project.
+//
+
+import SwiftUI
+import ReadiumShared
+
+struct OPDSFacetLInkView: View {
+    let link: ReadiumShared.Link
+    
+    var body: some View {
+        HStack {
+            if let title = link.title {
+                Text(title)
+                    .foregroundStyle(Color.primary)
+                    .font(.body)
+            }
+            
+            Spacer()
+            
+            if let count = link.properties.numberOfItems {
+                Text("\(count)")
+                    .foregroundStyle(Color.secondary)
+                    .font(.subheadline)
+            }
+            
+            Image(systemName: "chevron.right")
+                .font(.system(size: 14))
+        }
+    }
+}
+
+#Preview {
+    OPDSFacetLInkView(
+        link: Feed.preview.facets[0].links[0]
+    )
+    .padding()
+}
