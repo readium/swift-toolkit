4cfcc1982f51d4eabb2ef32f7967ef0a1812d519
Mickaël Menu <mickael.menu@gmail.com>
2025-01-03 12:29:12 +0100

    Update file headers

--- BuildTools/Package.swift
+++ BuildTools/Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version:5.3
 //
-//  Copyright 2021 Readium Foundation. All rights reserved.
+//  Copyright 2025 Readium Foundation. All rights reserved.
 //  Use of this source code is governed by the BSD-style license
 //  available in the top-level LICENSE file of the project.
 //
--- Sources/Navigator/Toolkit/Extensions/Bundle.swift
+++ Sources/Navigator/Toolkit/Extensions/Bundle.swift
@@ -1,14 +1,9 @@
 //
-//  Copyright 2024 Readium Foundation. All rights reserved.
+//  Copyright 2025 Readium Foundation. All rights reserved.
 //  Use of this source code is governed by the BSD-style license
 //  available in the top-level LICENSE file of the project.
 //
 
-//
-//  Copyright 2021 Readium Foundation. All rights reserved.
-//  Use of this source code is governed by the BSD-style license
-//  available in the top-level LICENSE file of the project.
-//
 import Foundation
 
 #if !SWIFT_PACKAGE
