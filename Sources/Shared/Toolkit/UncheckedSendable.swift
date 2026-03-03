c0f062411d3db6cfd0258a0d2b2f1855677fa28b
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2026-03-03 10:50:52 -0600

    Mark UncheckedSendable as @_spi

--- Sources/Shared/Toolkit/UncheckedSendable.swift
+++ Sources/Shared/Toolkit/UncheckedSendable.swift
@@ -9,7 +9,7 @@ import Foundation
 /// A wrapper to force a value to be `Sendable`.
 ///
 /// **Warning**: Use this wrapper only if you are sure that the value is thread-safe.
-public struct UncheckedSendable<T>: @unchecked Sendable {
+@_spi(Internal) public struct UncheckedSendable<T>: @unchecked Sendable {
     public let value: T
 
     public init(_ value: T) {
