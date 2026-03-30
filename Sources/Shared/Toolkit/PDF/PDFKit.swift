4640017d4055065c777264cbea7a6aa2d8ff26bc
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2026-03-30 17:04:28 -0500

    Add missed class to final class Sendable file

--- Sources/Shared/Toolkit/PDF/PDFKit.swift
+++ Sources/Shared/Toolkit/PDF/PDFKit.swift
@@ -52,7 +52,7 @@ extension PDFKit.PDFDocument: PDFDocument {
 }
 
 /// Creates a `PDFDocument` using PDFKit.
-public class PDFKitPDFDocumentFactory: PDFDocumentFactory {
+public final class PDFKitPDFDocumentFactory: PDFDocumentFactory, Sendable {
     public func open(file: FileURL, password: String?) async throws -> PDFDocument {
         guard let document = PDFKit.PDFDocument(url: file.url) else {
             throw PDFDocumentError.openFailed
