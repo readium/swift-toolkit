b10f767c0664dc1cbf0e8cc5ff13c7aa911e1c57
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2026-04-08 18:45:27 -0500

    Make AudioLocatorService Sendable

--- Sources/Streamer/Parser/Audio/Services/AudioLocatorService.swift
+++ Sources/Streamer/Parser/Audio/Services/AudioLocatorService.swift
@@ -13,26 +13,51 @@ final class AudioLocatorService: DefaultLocatorService {
         { context in AudioLocatorService(publication: context.publication) }
     }
 
-    private lazy var readingOrder: [Link] =
-        publication()?.readingOrder ?? []
+    private actor Cache {
+        let readingOrder: [Link]
+        let durations: [Double]
+        let totalDuration: Double?
 
-    /// Duration per reading order index.
-    private lazy var durations: [Double] =
-        readingOrder.map { $0.duration ?? 0 }
+        init(publication: Publication?) {
+            readingOrder = publication?.readingOrder ?? []
+            durations = readingOrder.map { $0.duration ?? 0 }
+            let total = durations.reduce(0, +)
+            totalDuration = (total > 0) ? total : nil
+        }
 
-    /// Total duration of the publication.
-    private lazy var totalDuration: Double? = {
-        let totalDuration = durations.reduce(0, +)
-        return (totalDuration > 0) ? totalDuration : nil
-    }()
+        func readingOrderItemAtPosition(_ position: Double) -> (link: Link, startPosition: Double)? {
+            var current: Double = 0
+            for (i, duration) in durations.enumerated() {
+                let link = readingOrder[i]
+                if current ..< current + duration ~= position {
+                    return (link, startPosition: current)
+                }
+
+                current += duration
+            }
+
+            if position == totalDuration, let link = readingOrder.last {
+                return (link, startPosition: current - (link.duration ?? 0))
+            }
+
+            return nil
+        }
+    }
+
+    private let cache: Cache
+
+    override init(publication: Weak<Publication>) {
+        cache = Cache(publication: publication())
+        super.init(publication: publication)
+    }
 
     override func locate(progression: Double) async -> Locator? {
-        guard let totalDuration = totalDuration else {
+        guard let totalDuration = cache.totalDuration else {
             return nil
         }
 
         let positionInPublication = progression * totalDuration
-        guard let (link, resourcePosition) = readingOrderItemAtPosition(positionInPublication) else {
+        guard let (link, resourcePosition) = await cache.readingOrderItemAtPosition(positionInPublication) else {
             return nil
         }
 
@@ -53,25 +78,5 @@ final class AudioLocatorService: DefaultLocatorService {
                 totalProgression: progression
             )
         )
-    }
-
-    /// Finds the reading order item containing the time `position` (in seconds), as well as its
-    /// start time.
-    private func readingOrderItemAtPosition(_ position: Double) -> (link: Link, startPosition: Double)? {
-        var current: Double = 0
-        for (i, duration) in durations.enumerated() {
-            let link = readingOrder[i]
-            if current ..< current + duration ~= position {
-                return (link, startPosition: current)
-            }
-
-            current += duration
-        }
-
-        if position == totalDuration, let link = readingOrder.last {
-            return (link, startPosition: current - (link.duration ?? 0))
-        }
-
-        return nil
     }
 }
