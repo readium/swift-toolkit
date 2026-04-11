5403764148beb752cecec1a0635e661c73d3b1eb
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2026-04-11 18:04:59 -0500

    Introduce AnySendableHashable

---
+++ Sources/Shared/Toolkit/AnySendableHashable.swift
@@ -0,0 +1,33 @@
+//
+//  Copyright 2026 Readium Foundation. All rights reserved.
+//  Use of this source code is governed by the BSD-style license
+//  available in the top-level LICENSE file of the project.
+//
+
+import Foundation
+
+/// A type-erased wrapper for a value that is both `Hashable` and `Sendable`.
+public struct AnySendableHashable: Hashable, Sendable {
+    public let base: any Hashable & Sendable
+    private let equals: @Sendable (any Hashable & Sendable) -> Bool
+    private let hasher: @Sendable (inout Hasher) -> Void
+
+    public init<T: Hashable & Sendable>(_ base: T) {
+        self.base = base
+        self.equals = { ($0 as? T) == base }
+        self.hasher = { base.hash(into: &$0) }
+    }
+
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.equals(rhs.base)
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        self.hasher(&hasher)
+    }
+
+    /// Safely unwraps the underlying value to the expected type.
+    public func unwrap<T: Hashable & Sendable>(as type: T.Type = T.self) -> T? {
+        base as? T
+    }
+}
