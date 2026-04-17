f87f073197fa7fd36c4bcb3a7416bafb87f082d0
Grigor Hakobyan <grighakobian@gmail.com>
2026-04-17 13:16:45 +0400

    Fix linting issue

--- Sources/Internal/Extensions/Date+ISO8601.swift
+++ Sources/Internal/Extensions/Date+ISO8601.swift
@@ -27,7 +27,6 @@ public extension DateFormatter {
         // Otherwise it will accept bad format, for exmaple 2018-04-24XXXXXXXXX
         // Because it will only test the part you asssigned, date, time, timezone.
         // But we should also cover the optional cases. So there is not too much benefit.
-        
         // https://developer.apple.com/documentation/foundation/dateformatter
         // Doesn't support millisecond or uncompleted part for date, time, timezone offset.
         let formats = [
