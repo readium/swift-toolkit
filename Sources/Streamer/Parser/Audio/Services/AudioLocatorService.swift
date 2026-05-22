5daf697dd90d0d3fa0adc96201b65b32864297e1
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2026-05-22 13:08:17 -0500

    Simplify state management

--- Sources/Streamer/Parser/Audio/Services/AudioLocatorService.swift
+++ Sources/Streamer/Parser/Audio/Services/AudioLocatorService.swift
@@ -9,61 +9,53 @@ import ReadiumShared
 
 /// Locator service for audio publications.
 final class AudioLocatorService: DefaultLocatorService {
-    static func makeFactory() -> (PublicationServiceContext) -> AudioLocatorService {
+    static func makeFactory() -> @Sendable (PublicationServiceContext) -> AudioLocatorService {
         { context in AudioLocatorService(publication: context.publication) }
     }
 
-    private struct Cache: Sendable {
-        let readingOrder: [Link]
-
-        /// Duration per reading order index.
-        let durations: [Double]
-
-        /// Total duration of the publication.
-        let totalDuration: Double?
-
-        init(publication: Publication?) {
-            readingOrder = publication?.readingOrder ?? []
-            durations = readingOrder.map { $0.duration ?? 0 }
-            let total = durations.reduce(0, +)
-            totalDuration = (total > 0) ? total : nil
-        }
-
-        /// Finds the reading order item containing the time `position` (in seconds), as well as its
-        /// start time.
-        func readingOrderItemAtPosition(_ position: Double) -> (link: Link, startPosition: Double)? {
-            var current: Double = 0
-            for (i, duration) in durations.enumerated() {
-                let link = readingOrder[i]
-                if current ..< current + duration ~= position {
-                    return (link, startPosition: current)
-                }
-
-                current += duration
-            }
-
-            if position == totalDuration, let link = readingOrder.last {
-                return (link, startPosition: current - (link.duration ?? 0))
-            }
-
-            return nil
-        }
-    }
-
-    private let cache: Cache
+    private let readingOrder: [Link]
+    private let durations: [Double]
+    private let totalDuration: Double?
 
     override init(publication: Weak<Publication>) {
-        cache = Cache(publication: publication())
+        let pub = publication()
+        let readingOrder = pub?.readingOrder ?? []
+        self.readingOrder = readingOrder
+        let durations = readingOrder.map { $0.duration ?? 0 }
+        self.durations = durations
+        let total = durations.reduce(0, +)
+        totalDuration = (total > 0) ? total : nil
+
         super.init(publication: publication)
     }
 
+    /// Finds the reading order item containing the time `position` (in seconds), as well as its
+    /// start time.
+    private func readingOrderItemAtPosition(_ position: Double) -> (link: Link, startPosition: Double)? {
+        var current: Double = 0
+        for (i, duration) in durations.enumerated() {
+            let link = readingOrder[i]
+            if current ..< current + duration ~= position {
+                return (link, startPosition: current)
+            }
+
+            current += duration
+        }
+
+        if position == totalDuration, let link = readingOrder.last {
+            return (link, startPosition: current - (link.duration ?? 0))
+        }
+
+        return nil
+    }
+
     override func locate(progression: Double) async -> Locator? {
-        guard let totalDuration = cache.totalDuration else {
+        guard let totalDuration = totalDuration else {
             return nil
         }
 
         let positionInPublication = progression * totalDuration
-        guard let (link, resourcePosition) = cache.readingOrderItemAtPosition(positionInPublication) else {
+        guard let (link, resourcePosition) = readingOrderItemAtPosition(positionInPublication) else {
             return nil
         }
 
