9ab2ae24b736556be9bceec9e44cd62c42e8b238
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2026-05-22 13:08:08 -0500

    Eagerly calculate positions

--- Sources/Shared/Publication/Services/Positions/PerResourcePositionsService.swift
+++ Sources/Shared/Publication/Services/Positions/PerResourcePositionsService.swift
@@ -9,53 +9,28 @@ import Foundation
 /// Simple `PositionsService` for a `Publication` which generates one position per `readingOrder`
 /// resource.
 public final class PerResourcePositionsService: PositionsService, Sendable {
-    private let readingOrder: [Link]
-
-    /// Media type that will be used as a fallback if the `Link` doesn't specify any.
-    private let fallbackMediaType: MediaType
+    private let positions: [[Locator]]
 
     init(readingOrder: [Link], fallbackMediaType: MediaType) {
-        self.readingOrder = readingOrder
-        self.fallbackMediaType = fallbackMediaType
-    }
-
-    public func positionsByReadingOrder() async -> ReadResult<[[Locator]]> {
-        .success(await cache.getPositions(readingOrder: readingOrder, fallbackMediaType: fallbackMediaType))
-    }
-
-    private actor Cache {
-        var positions: [[Locator]]?
-
-        func getPositions(readingOrder: [Link], fallbackMediaType: MediaType) -> [[Locator]] {
-            if let positions = positions {
-                return positions
-            }
-
-            guard !readingOrder.isEmpty else {
-                return []
-            }
-
-            let pageCount = readingOrder.count
-            let newPositions: [[Locator]] = readingOrder.enumerated().map { index, link in
-                [
-                    Locator(
-                        href: link.url(),
-                        mediaType: link.mediaType ?? fallbackMediaType,
-                        title: link.title,
-                        locations: Locator.Locations(
-                            totalProgression: Double(index) / Double(pageCount),
-                            position: index + 1
-                        )
-                    ),
-                ]
-            }
-
-            positions = newPositions
-            return newPositions
+        let pageCount = readingOrder.count
+        positions = readingOrder.enumerated().map { index, link in
+            [
+                Locator(
+                    href: link.url(),
+                    mediaType: link.mediaType ?? fallbackMediaType,
+                    title: link.title,
+                    locations: Locator.Locations(
+                        totalProgression: pageCount > 0 ? Double(index) / Double(pageCount) : 0.0,
+                        position: index + 1
+                    )
+                ),
+            ]
         }
     }
 
-    private let cache = Cache()
+    public func positionsByReadingOrder() async -> ReadResult<[[Locator]]> {
+        .success(positions)
+    }
 
     public static func makeFactory(fallbackMediaType: MediaType) -> @Sendable (PublicationServiceContext) -> PerResourcePositionsService {
         { context in
