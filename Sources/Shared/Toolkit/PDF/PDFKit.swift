6d1215bb6a6bdb36b4609392ce000f1021e3c4d4
Grigor Hakobyan <grighakobian@gmail.com>
2026-06-16 13:19:27 +0400

    Fix PDFDocument Sendable conformances for Swift 6

--- Sources/Shared/Toolkit/PDF/PDFKit.swift
+++ Sources/Shared/Toolkit/PDF/PDFKit.swift
@@ -25,7 +25,7 @@ extension PDFKit.PDFDocument: PDFKitDocumentProviding {
 /// If this is an issue for you, use `CPDFDocumentFactory` instead.
 ///
 /// Use `PDFKitPDFDocumentFactory` to create a `PDFDocument` from a `Resource`.
-extension PDFKit.PDFDocument: PDFDocument {
+extension PDFKit.PDFDocument: @retroactive @unchecked Sendable, PDFDocument {
     public func pageCount() async throws -> Int {
         pageCount
     }
