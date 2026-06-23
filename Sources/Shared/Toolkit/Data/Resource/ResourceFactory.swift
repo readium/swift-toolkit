29ea99a056ebc60197bbbe81693bbc15904a4354
Grigor Hakobyan <grighakobian@gmail.com>
2026-06-23 17:26:55 +0400

    Migrate Resource Factory

--- Sources/Shared/Toolkit/Data/Resource/ResourceFactory.swift
+++ Sources/Shared/Toolkit/Data/Resource/ResourceFactory.swift
@@ -7,7 +7,7 @@
 import Foundation
 
 /// A factory to create ``Resource`` instances from absolute URLs.
-public protocol ResourceFactory {
+public protocol ResourceFactory: Sendable {
     /// Creates a ``Resource`` to access the content at `url`.
     func make(url: AbsoluteURL) async -> Result<Resource, ResourceMakeError>
 }
@@ -19,7 +19,9 @@ public enum ResourceMakeError: Error, Sendable {
 
 /// Default implementation of ``ResourceFactory`` supporting file and http
 /// schemes.
-public final class DefaultResourceFactory: CompositeResourceFactory {
+public final class DefaultResourceFactory: ResourceFactory {
+    private let factory: CompositeResourceFactory
+
     /// - Parameters:
     ///   - httpClient: HTTP client used to support HTTP schemes.
     ///   - additionalFactories: Additional ``ResourceFactory`` to support more
@@ -28,16 +30,20 @@ public final class DefaultResourceFactory: CompositeResourceFactory {
         httpClient: HTTPClient,
         additionalFactories: [ResourceFactory] = []
     ) {
-        super.init(additionalFactories + [
+        self.factory = CompositeResourceFactory(additionalFactories + [
             FileResourceFactory(),
             HTTPResourceFactory(client: httpClient),
         ])
+    }
+
+    public func make(url: any AbsoluteURL) async -> Result<any Resource, ResourceMakeError> {
+        await factory.make(url: url)
     }
 }
 
 /// A composite ``ResourceFactory`` which tries several factories until it
 /// finds one which supports the URL scheme.
-public class CompositeResourceFactory: ResourceFactory {
+public final class CompositeResourceFactory: ResourceFactory {
     private let factories: [ResourceFactory]
 
     public init(_ factories: [ResourceFactory]) {
